b0468477bef8bac4bee47237fd9abc1ef83fbb9b
Lester Vecsey <service@paychess.com>
2012-04-09 08:55:16 -0400

    remove send_etherwake from original file

--- src/process.c
+++ src/process.c
@@ -11,23 +11,7 @@
 
 #include "udp6_shutdown.h"
 
-int send_etherwake(char *interface, unsigned char *mac_address) {
-
-  char string[50];
-
-  int retval;
-
-  sprintf(string, "etherwake -i %s %x:%x:%x:%x:%x:%x", interface, mac_address, mac_address+1, mac_address+2, mac_address+3, mac_address+4, mac_address+5);
-
-  retval = system(string);
-
-  if (retval!=0) {
-    fprintf(stderr, "%s: Tried to startup mac address and failed. retval=%d\n", __FUNCTION__, retval);
-  }
-
-  return retval;
-
-}
+#include "send_etherwake.h"
 
 int process(int s, char *interface, clusterboot_t *items, int num_entries) {
 
